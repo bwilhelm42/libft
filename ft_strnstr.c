ed6ea7f4c1cde8b55b278db897b15e228870679b
Brendan Wilhelmsen <bwilhelm@e1z3r6p8.42.us.org>
2020-02-23 14:23:00 -0800

    hopefully fixed a seg fault in strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bwilhelm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 21:08:41 by bwilhelm          #+#    #+#             */
-/*   Updated: 2020/02/21 21:50:22 by bwilhelm         ###   ########.fr       */
+/*   Updated: 2020/02/23 14:22:22 by bwilhelm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	small = (char*)needle;
 	if (*needle == '\0')
 		return (big);
+	if (len < ft_strlen(small))
+		return (NULL);
 	while (*big != '\0' && len-- > 0)
 	{
 		i = 0;
