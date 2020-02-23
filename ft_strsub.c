625beb27ab0bd329cf35e7965f1f6f253ec2ca9b
Brendan Wilhelmsen <bwilhelm@e1z3r6p8.42.us.org>
2020-02-23 14:59:21 -0800

    protecting malloc return

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: bwilhelm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 17:21:51 by bwilhelm          #+#    #+#             */
-/*   Updated: 2020/02/20 18:59:07 by bwilhelm         ###   ########.fr       */
+/*   Updated: 2020/02/23 14:58:04 by bwilhelm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	char *str;
 
 	str = &((char*)s)[start];
-	str = (char*)malloc(len);
+	if((str = (char*)malloc(len)) == NULL)
+		return NULL;
 	str[2] = 'A';
 	return (str);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: bwilhelm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 19:34:09 by bwilhelm          #+#    #+#             */
-/*   Updated: 2020/02/21 21:53:37 by bwilhelm         ###   ########.fr       */
+/*   Updated: 2020/02/23 14:58:59 by bwilhelm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,7 @@ char			*ft_strtrim(char const *s)
 
 	ptr = (char*)s;
 	i = cut_spaces(ptr);
-	str = (char*)malloc(i + 1);
-	if (str == NULL)
+	if((str = (char*)malloc(i + 1)) == NULL)
 		return (NULL);
 	while ((*ptr == ' ' || *ptr == '\n' || *ptr == '\t')
 			&& *ptr != '\0')
