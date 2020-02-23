a95d56b6d92eddfdc99e848d00b58f028487b029
Brendan Wilhelmsen <bwilhelm@e1z3r6p8.42.us.org>
2020-02-23 14:53:57 -0800

    turns out strnstr seg faults when both strings are the same size and len is exactly equal, fixed that

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bwilhelm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 21:08:41 by bwilhelm          #+#    #+#             */
-/*   Updated: 2020/02/23 14:36:29 by bwilhelm         ###   ########.fr       */
+/*   Updated: 2020/02/23 14:52:55 by bwilhelm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,19 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	big = (char*)haystack;
 	small = (char*)needle;
-	if (big == NULL)
-		return NULL;
-	if (small == NULL || *small == '\0')
+	if (*small == '\0')
 		return (big);
-	while (*big != '\0' && len-- > 0)
+	while (*big != '\0' && len > 0)
 	{
 		i = 0;
-		while (big[i] == needle[i] && len - i > 0)
+		while ((big[i] == small[i] && len - i > 0))
 		{
 			i++;
-			if (needle[i] == '\0')
+			if (small[i] == '\0')
 				return (big);
 		}
 		big++;
+		len--;
 	}
 	return (NULL);
 }
