22ec7f0705ac3ce7d6387da022a16ea0924861a8
brendan <brendanrayw@gmail.com>
2020-05-30 01:57:15 -0700

    fixed function name and added executable to gitignore

--- libft.h
+++ libft.h
@@ -84,7 +84,7 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstiter(t_list *lst, void (*f)(void*));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void*));
 void				ft_lstadd_front(t_list **lst, t_list *new);
-void				ft_lstaddback(t_list **alst, t_list *new);
+void				ft_lstadd_back(t_list **alst, t_list *new);
 int					ft_lstsize(t_list *lst);
 void				ft_strrep(char *str, char new, char old);
 void				ft_putstrarr(char **arr, int n);
